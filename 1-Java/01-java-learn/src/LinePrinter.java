567ab3aba933fa117d54bc5df552732642d095ee
Sibghatullah <mdsibghat28@gmail.com>
2021-09-01 12:56:49 +0530

    line printer

--- 1-Java/01-java-learn/src/LinePrinter.java
+++ 1-Java/01-java-learn/src/LinePrinter.java
@@ -3,11 +3,20 @@ public class LinePrinter {
 
 	public static void main(String[] args) {
 		printLine();
+		printLine(15);
+		printLine(30);
 
 	}
 	
 	public static void printLine() {
 		System.out.println("------------------------------");
+	}
+	
+	public static void printLine(int numberOfCharacters) {
+		for (int i = 0; i < numberOfCharacters; i++) {
+			System.out.print("-");
+		}
+		System.out.println();
 	}
 
 }
