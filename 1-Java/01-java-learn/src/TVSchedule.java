30b6c8014ee2137dafe794aad3b250a7ae24f951
Sibghatullah <mdsibghat28@gmail.com>
2021-09-01 11:38:47 +0530

    day 3

--- 1-Java/01-java-learn/src/TVSchedule.java
+++ 1-Java/01-java-learn/src/TVSchedule.java
@@ -3,11 +3,19 @@ public class TVSchedule {
 
 	public static void main(String[] args) {
 		int date = 2;
-		while (date <= 31) {
+//		while (date <= 31) {
+//			System.out.println(date);
+//			date += 3;
+//		}
+		
+//		do {
+//			System.out.println(date);
+//			date += 3;
+//		} while (date <= 31);
+		
+		for (date = 2; date <= 31; date += 3) {
 			System.out.println(date);
-			date += 3;
 		}
-
 	}
 
 }
