490431b6da6e27ec4ffaaedcfd3ebc006cb36c46
Sibghatullah <mdsibghat28@gmail.com>
2021-09-01 18:23:39 +0530

    accounts

--- 1-Java/01-java-learn/src/AccManager.java
+++ 1-Java/01-java-learn/src/AccManager.java
@@ -2,7 +2,23 @@
 public class AccManager {
 
 	public static void main(String[] args) {
-		System.out.println();calculateBalanceAndPrint('W', 2000.0, 5000.0);
+		String accountNumber = "1234567890";
+		double balance = 5000.0;
+		
+		System.out.println("Account Statement of " + accountNumber);
+		System.out.println("Initial Balance: " + balance);
+		
+		System.out.println("\n--------------------------");
+		System.out.println("Type  Transaction  Balance");
+		System.out.println("--------------------------");
+		
+		
+		balance = calculateBalanceAndPrint('W', 2000.0, balance);
+		balance = calculateBalanceAndPrint('D', 20000.0, balance);
+		balance = calculateBalanceAndPrint('D', 500.0, balance);
+		balance = calculateBalanceAndPrint('W', 15500.0, balance);
+		balance = calculateBalanceAndPrint('W', 10000.0, balance);
+		
 
 	}
 	public static double calculateBalanceAndPrint(char type, double transaction, double balance) {
@@ -12,6 +28,7 @@ public class AccManager {
 		} else if (type == 'D'){
 			balance += transaction;
 		}
+		System.out.printf("%4c %12.2f %8.2f\n", type, transaction, balance);
 		return balance;
 	}
 
